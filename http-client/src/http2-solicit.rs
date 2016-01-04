da57117a45b80d196f064ef31e20993b48c4ea71
hhatto <hhatto.jp@gmail.com>
2016-01-05 00:46:45 +0900

    add http2 client

---
+++ http-client/src/http2-solicit.rs
@@ -0,0 +1,14 @@
+extern crate solicit;
+use solicit::http::client::CleartextConnector;
+use solicit::client::SimpleClient;
+use std::str;
+
+static HOST: &'static str = "http2bin.org";
+
+fn main() {
+    let connector = CleartextConnector::new(HOST);
+    let mut client = SimpleClient::with_connector(connector).unwrap();
+    let resp = client.get(b"/", &[]).unwrap();
+
+    println!("{}", str::from_utf8(&resp.body).unwrap());
+}
