87467d0c518ea1faf85dcb67099874b1cce849e6
hhatto <hhatto.jp@gmail.com>
2016-01-05 00:48:59 +0900

    rustfmt & remove not needed line

--- http-client/src/get-json-hyper.rs
+++ http-client/src/get-json-hyper.rs
@@ -1,16 +1,9 @@
 extern crate hyper;
 extern crate rustc_serialize;
 use std::io::Read;
-use std::collections::HashMap;
-
 use rustc_serialize::json::Json;
 use hyper::Client;
 use hyper::header::Connection;
-
-#[derive(Debug, RustcDecodable, RustcEncodable)]
-struct QiitaResp {
-    id: HashMap<String, String>
-}
 
 static URL: &'static str = "http://qiita.com/api/v2/items?per_page=2";
 
@@ -21,26 +14,27 @@ pub fn parse_json(jsonstr: &String) {
     for d in items.iter() {
         let item = d.as_object().unwrap().get("title").unwrap();
         let datetime = d.as_object().unwrap().get("updated_at").unwrap();
-        println!("{} {}", match *datetime {
-            Json::String(ref v) => format!("{}", v),
-            _ => format!(""),
-        },
-        match *item {
-            Json::String(ref v) => format!("{}", v),
-            _ => format!(""),
-        });
+        println!("{} {}",
+                 match *datetime {
+                     Json::String(ref v) => format!("{}", v),
+                     _ => format!(""),
+                 },
+                 match *item {
+                     Json::String(ref v) => format!("{}", v),
+                     _ => format!(""),
+                 });
     }
 }
 
 fn main() {
     let client = Client::new();
     let mut res = client.get(URL)
-        .header(Connection::close())
-        .send().unwrap();
+                        .header(Connection::close())
+                        .send()
+                        .unwrap();
 
     let mut body = String::new();
     res.read_to_string(&mut body).unwrap();
 
-    //println!("Resp: {}", body);
     parse_json(&body);
 }
