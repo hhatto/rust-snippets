d4cf540562686c5683fc01a5106702928d6894a3
Hideo Hattori <hattori-h@klab.com>
2016-07-12 18:49:01 +0900

    remove not needed line

--- networking/src/unix-domain-socket-client.rs
+++ networking/src/unix-domain-socket-client.rs
@@ -2,34 +2,10 @@ extern crate unix_socket;
 
 use std::env;
 use std::io::prelude::*;
-use std::io::Cursor;
 use unix_socket::UnixStream;
 
-struct Header {
-    ver: i8,
-    psh: i8,
-    len: i16,
-    offset: i32,
-    buf_len: i32,
-}
-
-impl Header {
-    fn new() -> Header {
-        Header {
-            ver: 1,
-            psh: 1,
-            len: 12,
-            offset: 0,
-            buf_len: 0,
-        }
-    }
-
-    fn bytes(&self) {}
-}
-
 fn main() {
-    let mut socket_path = env::args().skip(1).next().unwrap();
+    let socket_path = env::args().skip(1).next().unwrap();
     let mut stream = UnixStream::connect(socket_path.as_str()).unwrap();
-    let h = Header::new();
     stream.write_all(b"hello").unwrap();
 }
