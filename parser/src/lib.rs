ae696c9ecc24bea9a90475ffc10102b97ca44267
Hideo Hattori <hattori-h@klab.com>
2016-07-19 21:39:47 +0900

    update woothee snippet

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,22 +1,24 @@
 // #![feature(test)]
 // extern crate uap_rust;
 // extern crate woothee;
-// use uap_rust::parser::Parser;
-// use woothee::parse;
+// use uap_rust::parser as uap;
+// use woothee::parser as woo;
 //
 // pub fn b_uap() {
-//    let parser = Parser::new().unwrap();
-//    let _ = parser.parse("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)"
-//                             .to_string());
+//    let parser = uap::Parser::new().unwrap();
+//    let _ =
+//        parser.parse("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)".to_string());
 //    let _ = parser.parse("Twitterbot/1.0".to_string());
-//    let _ = parser.parse("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; Xbox)"
-//                             .to_string());
+//    let _ =
+//        parser.parse("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; Xbox)"
+//            .to_string());
 // }
 //
 // pub fn b_woothee() {
-//    let _ = parse("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)");
-//    let _ = parse("Twitterbot/1.0");
-//    let _ = parse("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; Xbox)");
+//    let parser = woo::Parser::new();
+//    let _ = parser.parse("Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)");
+//    let _ = parser.parse("Twitterbot/1.0");
+//    let _ = parser.parse("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0; Xbox)");
 // }
 //
 // extern crate test;
@@ -29,4 +31,9 @@
 // #[bench]
 // fn bench_woothee(b: &mut Bencher) {
 //    b.iter(|| b_woothee());
+// }
+//
+// #[bench]
+// fn bench_stabilizer(b: &mut Bencher) {
+//    b.iter(|| "hoge".contains("h"));
 // }
