fab28045c636e2504f2617647b0ff1f8b7d5d695
Hideo Hattori <hattori-h@klab.com>
2018-03-27 15:47:39 +0900

    update pyo3 and rust-cpython

--- extension/benchmark/bench.py
+++ extension/benchmark/bench.py
@@ -2,8 +2,6 @@ from benchmarker import Benchmarker
 import fast_woothee_pyo3
 import fast_woothee_cpy
 
-pyo3_parse = fast_woothee_pyo3.parse
-cpy_parse = fast_woothee_cpy.parse
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.104 Safari/537.36'
 
 
@@ -11,14 +9,38 @@ n = 1000 * 1000
 
 with Benchmarker(n) as bench:
 
-    @bench("rust-cpython")
+    @bench("(rust-cpython) parse")
     def _cpython(bm):
-        parse = cpy_parse
+        parse = fast_woothee_cpy.parse
         for i in bm:
             parse(ua)
 
-    @bench("pyo3")
+    @bench("(pyo3) parse")
     def _pyo3(bm):
-        parse = pyo3_parse
+        parse = fast_woothee_pyo3.parse
+        for i in bm:
+            parse(ua)
+
+    @bench("(rust-cpython) parse2")
+    def _cpython(bm):
+        parse = fast_woothee_cpy.parse2
+        for i in bm:
+            parse(ua)
+
+    @bench("(pyo3) parse2")
+    def _pyo3(bm):
+        parse = fast_woothee_pyo3.parse2
+        for i in bm:
+            parse(ua)
+
+    @bench("(rust-cpython) is_crawler")
+    def _cpython(bm):
+        parse = fast_woothee_cpy.is_crawler
+        for i in bm:
+            parse(ua)
+
+    @bench("(pyo3) is_crawler")
+    def _pyo3(bm):
+        parse = fast_woothee_pyo3.is_crawler
         for i in bm:
             parse(ua)
--- extension/pyo3/src/lib.rs
+++ extension/pyo3/src/lib.rs
@@ -29,6 +29,27 @@ fn init_mod(py: Python, m: &PyModule) -> PyResult<()> {
         Ok(h)
     }
 
+    #[pyfn(m, "parse2")]
+    pub fn parse2(agent: &str) -> PyResult<PyObject> {
+        let parser = Parser::new();
+        let result = parser.parse(agent);
+        let r = match result {
+            Some(r) => r,
+            None => WootheeResult::new(),
+        };
+        let gil = Python::acquire_gil();
+        let py = gil.python();
+        let h = PyDict::new(py);
+        let _ = h.set_item(PyString::new(py, "name"), PyString::new(py, r.name.as_str()));
+        let _ = h.set_item(PyString::new(py, "category"), PyString::new(py, r.category.as_str()));
+        let _ = h.set_item(PyString::new(py, "os"), PyString::new(py, r.os.as_str()));
+        let _ = h.set_item(PyString::new(py, "os_version"), PyString::new(py, r.os_version.as_str()));
+        let _ = h.set_item(PyString::new(py, "browser_type"), PyString::new(py, r.browser_type.as_str()));
+        let _ = h.set_item(PyString::new(py, "version"), PyString::new(py, r.version.as_str()));
+        let _ = h.set_item(PyString::new(py, "vendor"), PyString::new(py, r.vendor.as_str()));
+        Ok(h.to_object(py))
+    }
+
     #[pyfn(m, "is_crawler")]
     pub fn is_crawler(agent: &str) -> PyResult<bool> {
         if agent.is_empty() || agent == "-" {
