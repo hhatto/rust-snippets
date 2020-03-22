c2d423d663d73b506b0c3e2f77991e0e69ed9cd1
Hideo Hattori <hhatto.jp@gmail.com>
2020-03-22 22:12:51 +0900

    Tera 1.1 and edition 2018

--- template-engine/src/tera.rs
+++ template-engine/src/tera.rs
@@ -1,4 +1,3 @@
-extern crate tera;
 use std::env;
 use std::path::Path;
 use tera::{Tera, Context};
@@ -6,14 +5,17 @@ use tera::{Tera, Context};
 fn main() {
     let root_path = env::current_dir().unwrap();
     let template_dir = root_path.join(Path::new("templates/*"));
-    let template_engine = Tera::new(template_dir.to_str().unwrap());
+    let template_engine = match Tera::new(template_dir.to_str().unwrap()) {
+        Ok(t) => t,
+        Err(e) => panic!("tera::new error: {}", e),
+    };
     let mut context = Context::new();
 
     let foo = "";
     let hello = "world";
     let vector = vec![1, 3, 6];
-    context.add("foo", &foo);
-    context.add("hello", &hello);
-    context.add("vector", &vector);
-    println!("{}", template_engine.render("hello.tera", context).unwrap());
+    context.insert("foo", &foo);
+    context.insert("hello", &hello);
+    context.insert("vector", &vector);
+    println!("{}", template_engine.render("hello.tera", &context).unwrap());
 }
