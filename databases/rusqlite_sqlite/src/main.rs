f0738d1a74f5a9666867dfc9b42ab32c083c2cc6
Hideo Hattori <hhatto.jp@gmail.com>
2022-06-12 13:14:05 +0900

    update

--- databases/rusqlite_sqlite/src/main.rs
+++ databases/rusqlite_sqlite/src/main.rs
@@ -10,22 +10,21 @@ fn main() {
                   name            TEXT NOT NULL,
                   age             INTEGER NOT NULL,
                   created         TEXT NOT NULL
-                  )", &[]).unwrap();
+                  )", []).unwrap();
 
     let name = "rustacean";
-    let age = 20;
+    let age = "20";
     conn.execute("INSERT INTO person (name, age, created)
                   VALUES (?1, ?2, datetime())",
                  &[&name, &age]).unwrap();
 
     let mut stmt = conn.prepare("SELECT id, name, age, created FROM person").unwrap();
-    let mut cursor = stmt.query(&[]).expect("stmt.query() error");
-    while let Some(row) = cursor.next() {
-        let r = row.expect("result row error");
-        let id: i32 = r.get(0);
-        let name: String = r.get(1);
-        let age: i32 = r.get(2);
-        let created: String = r.get(3);
+    let mut cursor = stmt.query([]).expect("stmt.query() error");
+    while let Some(r) = cursor.next().unwrap() {
+        let id: i32 = r.get(0).unwrap();
+        let name: String = r.get(1).unwrap();
+        let age: i32 = r.get(2).unwrap();
+        let created: String = r.get(3).unwrap();
         println!("{:?}, {}, {}, {}", id, name, age, created);
     }
 }
